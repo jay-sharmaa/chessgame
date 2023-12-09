4e5851f4bbd588b00dead2b861a26e2b07c68dc9
jay-sharmaa <sjaysharma00@gmail.com>
2023-12-09 23:45:39 +0530

    Load up white images and set up move fnuction

--- chp4/emaildb.py
+++ chp4/emaildb.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # setting up screen
-screen = pygame.display.set_mode((900, 900))
+screen = pygame.display.set_mode((1000, 900))
 running = True
 
 pygame.display.set_caption("Chess Game")
@@ -13,18 +13,19 @@ icon = pygame.image.load("iconboard.png")
 
 pygame.display.set_icon(icon)
 
-bishop = pygame.image.load("bishop.png")
+king_white = pygame.image.load("king.png")
+bishop_white_left = pygame.image.load("bishop.png")
+knight_white_left = pygame.image.load("knight.png")
+rook_white_left = pygame.image.load("rook.png")
+queen_white = pygame.image.load("queen.png")
 
 
 def bishop_pieces(x, y):
-    screen.blit(bishop, (x, y))
+    screen.blit(bishop_white_left, (x, y))
 
 
 x_pos = 0
 y_pos = 800
-
-screen.fill((255, 255, 255))
-pygame.display.update()
 
 # game loop
 while running:
@@ -33,6 +34,8 @@ while running:
             running = False
     x_pos += 0
     y_pos -= 80
+    screen.fill((255, 255, 255))
+    pygame.display.update()
     bishop_pieces(x_pos,y_pos)
     pygame.display.update()
     pygame.time.wait(500)
